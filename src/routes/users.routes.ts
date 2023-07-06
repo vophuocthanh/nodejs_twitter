40acf5b17a3d4498fb1df2b3acf6170ab9adb849
Võ Phước Thạnh <phuocthanh2k03@gmail.com>
2023-07-06 21:53:17 +0700

    error handling

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -11,6 +11,37 @@ usersRouter.post('/login', loginValidator, loginController)
  * Method: POST
  * Body: {name: string, email: string, password: string, date_of_birth: ISO8601 (tiêu chuẩn quốc tế), confirm_password: string}
  */
-usersRouter.post('/register', RegisterValidator, RegisterController)
+usersRouter.post(
+  '/register',
+  RegisterValidator,
+  // request handler, nó tự next
+  async (req, res, next) => {
+    console.log('requests handler 1')
+    // next(new Error('Lỗi rồi bạn êi'))
+    // dùng asnyc thì nó không tự next mà chúng ta phải tự next, muốn next thì phải try catch
+    //  dùng try catch cũng được
+    // try {
+    //   throw new Error('Lỗi rồi bạn ơiiiiiiiii') // tương tự như next
+    // } catch (error) {
+    //   next(error)
+    // }
+    // 2 cách này đều được
+    // Promise.reject(new Error('Lỗi rồi nà')).catch((e) => next(e))
+    Promise.reject(new Error('Lỗi rồi nà')).catch(next)
+  },
+  (req, res, next) => {
+    console.log('requests handler 2')
+    next()
+  },
+  (req, res, next) => {
+    console.log('requests handler 3')
+    res.json({ message: 'User registration' })
+  },
+  // error handler
+  (err, req, res, next) => {
+    console.log('Lôi là, ', err.message)
+    res.status(400).json({ error: err.message })
+  }
+)
 
 export default usersRouter
